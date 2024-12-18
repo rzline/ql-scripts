867676cc3e14ff7e54bbed57bba10f612e7d1de7
任梓樂 <100897979+rzline@users.noreply.github.com>
2024-12-18 12:54:38 +0800

    Update littleskin.py

--- littleskin.py
+++ littleskin.py
@@ -1,24 +1,31 @@
 import os
 import requests
-from bs4 import BeautifulSoup
 import time
+from bs4 import BeautifulSoup
 
 # 获取账号和密码
 account = os.getenv('SKINACCOUNT')
-password = os.getenv('SKINPASSWORD')
+password = os.getenv('SKINPASSWD')
+TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
+TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+
 
 # 目标网站地址
 endpoint = 'https://littleskin.cn/'
 
+# 设置请求头
+headers = {
+    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.7,zh-TW;q=0.5,zh-HK;q=0.3,en;q=0.2",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+}
+
 # 初始化 session
 session = requests.Session()
+session.headers.update(headers)
 
-# 直接设置请求头
-with open('headers.json', 'r', encoding='utf-8') as f:
-    session.headers.update(json.load(f))
-
+# 执行任务
 def task():
-    # 登录页面获取 CSRF token
+    # 获取 CSRF token
     home_page = session.get(endpoint + 'auth/login')
     csrf_token = BeautifulSoup(home_page.text, 'html.parser').find('meta', {'name': 'csrf-token'})['content']
 
@@ -26,26 +33,37 @@ def task():
     login_data = {'identification': account, 'keep': False, 'password': password}
     session.post(endpoint + 'auth/login', data=login_data, headers={'X-CSRF-TOKEN': csrf_token})
 
-    # 获取用户页面，再次提取 CSRF token
+    # 获取用户页面，提取新的 CSRF token
     user_page = session.get(endpoint + 'user')
     csrf_token = BeautifulSoup(user_page.text, 'html.parser').find('meta', {'name': 'csrf-token'})['content']
 
     # 签到请求
     sign_response = session.post(endpoint + 'user/sign', headers={'X-CSRF-TOKEN': csrf_token})
-    print(sign_response.text)
 
+    # 打印并发送通知
+    print(sign_response.text)
+    telegram_notify("签到成功", sign_response.text)
+
+# 主函数
 def main():
-    max_retry = 3
-    for i in range(max_retry):
-        try:
-            task()
-            break
-        except Exception as e:
-            print(f"Attempt {i + 1} failed: {e}")
-            if i < max_retry - 1:
-                time.sleep(10)  # 等待 10 秒再重试
-            else:
-                raise
+    try:
+        task()
+    except Exception as e:
+        telegram_notify("任务失败", f"错误信息：{str(e)}")
+
+# 发送 Telegram 通知
+def telegram_notify(title, content=""):
+    if not TG_BOT_TOKEN or not TG_CHAT_ID:
+        print("未配置 Telegram 推送所需的环境变量")
+        return
+    
+    url = f"https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": TG_CHAT_ID, "text": f"{title}\n\n{content}", "parse_mode": "Markdown"}
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Telegram 推送失败：{e}")
 
 if __name__ == '__main__':
     main()
